c574c0d5a5be6be2c7af0937dbe9c60bf5d6232c
Makishimamo <maximinpony@gmail.com>
2024-06-13 17:25:08 +0200

    function addComment

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,41 @@ let nom = document.getElementById("last-name");
 let commentaire = document.getElementById("message");
 let form = document.getElementById("form");
 let errorMessage = document.getElementById("error-message");
+let commentList = document.getElementsById("comment-list");
+let bouton = document.getElementById("bouton");
 
+// Je crée une fonction ajout du commentaire
+function addComment() {
+    // Je crée une d1 dans commentList
+    let d1 = createElement("div");
+    d1.setAttribute("class", "flex space-x-4 text-sm text-gray-500");
+    commentList.appendChild(d1);
+    // Je crée une d2 dans d1
+    let d2 = createElement("div");
+    d2.setAttribute("class", "flex-1 py-10");
+    d1.appendChild(d2);
+    // Je crée une h3 dans d2
+    let newTitle = createElement("h3");
+    newTitle.setAttribute("class", "font-medium text-gray-900");
+    d2.appendChild(newTitle);
+    // Je crée une d3 dans d3
+    let d3 = createElement("div");
+    d3.setAttribute("class", "prose prose-sm mt-4 max-w-none text-gray-500");
+    d2.appendChild(d3);
+    // Je crée un paragraphe dans d3
+    let newParagraph = createElement("p");
+    d3.appendChild(newParagraph);
+    // Je récupère le prénom, le nom et je les insère dans newTitle
+    let newTitleContent = prenom.value + " " + nom.value;
+    newTitle.appendChild(newTitleContent);
+    // Je récupère le commentaire et je l'insère dans newParagraph
+    let newParagraphContent = commentaire.value;
+    newParagraph.appendChild(commentaire.value);
+}
+// Je lie ma fonction addComment au bouton HTML
+bouton.addEventListener("submit", addComment);
+
+/*
 // Vérifier les champs du formulaire
 try {
     if (!prenom) {
@@ -22,3 +56,4 @@ catch (err) {
     errorMessage.removeAttribute("style");
     console.log ("Erreur : " +err.message);
 }
+*/
