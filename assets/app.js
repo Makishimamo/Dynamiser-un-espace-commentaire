29460fbc69fd3733b25118dc4d9683978ff76209
Makishimamo <maximinpony@gmail.com>
2024-06-14 17:56:05 +0200

    synthax

--- assets/app.js
+++ assets/app.js
@@ -3,12 +3,12 @@ alert("Hello!")
 */
 
 // Je crée mes variables
-const prenom = document.getElementById("first-name");
-const nom = document.getElementById("last-name");
-const message = document.getElementById("message");
-const errorMessage = document.getElementById("error-message");
-const commentList = document.getElementById("comment-list");
-const bouton = document.getElementById("bouton");
+const prenom = document.querySelector("#first-name");
+const nom = document.querySelector("#last-name");
+const message = document.querySelector("#message");
+const errorMessage = document.querySelector("#error-message");
+const commentList = document.querySelector("#comment-list");
+const bouton = document.querySelector("#bouton");
 
 // Je récupère dans une variable la valeur de chaque champs et je vérifie dans la console
 prenom.addEventListener('input', function(e) {
@@ -37,20 +37,42 @@ message.addEventListener('input', function(e){
 
 // Je crée une fonction pour ajouter un commentaire
 function addComment() {
-    // Je crée le nouveau commentaire
-    const comment = `
-        <div class="flex space-x-4 text-sm text-gray-500"
-            <div class="flex-1 py-10 ">
-                <h3 class="font-medium text-gray-900">${prenomText} ${nomText}</h3>
-                <div class="prose prose-sm mt-4 max-w-none text-gray-500">
-                    <p>${messageText}</p>
-                </div>
-            </div>
-        </div>    
-    `
+    //Je crée mes éléments
+    let d1 = document.createElement("div");
+    let d2 = document.createElement("div");
+    let d3 = document.createElement("div");
+    let title = document.createElement("h3");
+    let para = document.createElement("p");
+    
+    // J'ajoute une class aux éléments
+    d1.setAttribute("class", "flex space-x-4 text-sm text-gray-500");
+    d2.setAttribute("class", "flex-1 py-10");
+    d3.setAttribute("class", "prose prose-sm mt-4 max-w-none text-gray-500");
+    title.setAttribute("class", "font-medium text-gray-900");
 
-    // J'ajoute le nouveau commentaire à la suite des autres
-    commentList.insertAdjacentHTML('beforeend', comment)
+    // Je définis le valeur des différents éléments
+    
+    // Je place mes éléments aux bons endroits
+    commentList.appendChild(d1);
+    d1.appendChild(d2);
+    d2.appendChild(title);
+    d2.appendChild(d3);
+    d3.appendChild(para);
+
+    // // Je crée le nouveau commentaire
+    // const comment = `
+    //     <div class="flex space-x-4 text-sm text-gray-500"
+    //         <div class="flex-1 py-10 ">
+    //             <h3 class="font-medium text-gray-900">${prenomText} ${nomText}</h3>
+    //             <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+    //                 <p>${messageText}</p>
+    //             </div>
+    //         </div>
+    //     </div>    
+    // `
+
+    // // J'ajoute le nouveau commentaire à la suite des autres
+    // commentList.insertAdjacentHTML('beforeend', comment)
   
 }
 
