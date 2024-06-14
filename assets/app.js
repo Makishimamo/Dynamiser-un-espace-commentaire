c44efc58ddd82df9d506fce2dde77f2e844fe25e
Makishimamo <maximinpony@gmail.com>
2024-06-14 17:30:55 +0200

    refactoring

--- assets/app.js
+++ assets/app.js
@@ -5,27 +5,33 @@ alert("Hello!")
 // Je crée mes variables
 const prenom = document.getElementById("first-name");
 const nom = document.getElementById("last-name");
-const text = document.getElementById("message");
+const message = document.getElementById("message");
 let errorMessage = document.getElementById("error-message");
 let commentList = document.getElementById("comment-list");
 
-// Je vérifie si les champs sont remplit
+// Je récupère dans une variable la valeur de chaque champs et je vérifie dans la console
 prenom.addEventListener('input', function(e) {
     if(e.target.value === "") {
         errorMessage.removeAttribute("style");
     }
+    let prenomText = e.target.value;
+    console.log(prenomText);
 })
 
 nom.addEventListener('input', function(e){
     if(e.target.value === "") {
         errorMessage.removeAttribute("style");
     }
+    let nomText = e.target.value;
+    console.log(nomText);
 })
 
-text.addEventListener('input', function(e){
+message.addEventListener('input', function(e){
     if(e.target.value === "") {
         errorMessage.removeAttribute("style");
     }
+    let messageText = e.target.value;
+    console.log(messageText);
 })
 
 // Je crée une fonction pour ajouter un commentaire
@@ -34,9 +40,9 @@ function addComment() {
     const comment = `
         <div class="flex space-x-4 text-sm text-gray-500"
             <div class="flex-1 py-10 ">
-                <h3 class="font-medium text-gray-900">${prenom.e.target.value} ${nom.e.target.value}</h3>
+                <h3 class="font-medium text-gray-900">${prenomText} ${nomText}</h3>
                 <div class="prose prose-sm mt-4 max-w-none text-gray-500">
-                    <p>${text.e.target.value}</p>
+                    <p>${messageText}</p>
                 </div>
             </div>
         </div>    
@@ -45,29 +51,6 @@ function addComment() {
     // J'ajoute le nouveau commentaire à la suite des autres
     commentList.insertAdjacentHTML('beforeend', comment)
   
-    /*
-    // Je crée une d2 dans d1
-    let d2 = createElement("div");
-    d2.setAttribute("class", "flex-1 py-10");
-    d1.appendChild(d2);
-    // Je crée une h3 dans d2
-    let newTitle = createElement("h3");
-    newTitle.setAttribute("class", "font-medium text-gray-900");
-    d2.appendChild(newTitle);
-    // Je crée une d3 dans d3
-    let d3 = createElement("div");
-    d3.setAttribute("class", "prose prose-sm mt-4 max-w-none text-gray-500");
-    d2.appendChild(d3);
-    // Je crée un paragraphe dans d3
-    let newParagraph = createElement("p");
-    d3.appendChild(newParagraph);
-    // Je récupère le prénom, le nom et je les insère dans newTitle
-    let newTitleContent = prenom.value + " " + nom.value;
-    newTitle.appendChild(newTitleContent);
-    // Je récupère le commentaire et je l'insère dans newParagraph
-    let newParagraphContent = commentaire.value;
-    newParagraph.appendChild(commentaire.value);
-    */
 }
 
 // Je lie ma fonction addComment au bouton HTML
@@ -76,25 +59,5 @@ document.getElementById("bouton").addEventListener("click", addComment);
 /* // Je test l'appel de ma fonction au clic
 function test() {
     alert("ok");
-}
-*/
-
-/*
-// Vérifier les champs du formulaire
-try {
-    if (!prenom) {
-        throw new Error ('Le prénom est requis');
-    } else if (!nom) {
-        throw new Error ('Le nom est requis');
-    } else if (!commentaire) {
-        throw new Error ('Le commentaire est requis');
-    } else if (commentaire.length > 500) {
-        throw new Error ('Le commentaire est trop long (500 maximum)');
-    }   
-} 
-// Gestion des erreurs
-catch (err) {
-    
-    console.log ("Erreur : " +err.message);
 }
 */
